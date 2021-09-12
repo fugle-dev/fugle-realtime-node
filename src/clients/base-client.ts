82d9b74c384a534b216d87bd18d136b5f9b84291
Chun-Kai Wang <chunkai1312@gmail.com>
2021-09-13 00:05:53 +0800

    chore: add comments

--- src/clients/base-client.ts
+++ src/clients/base-client.ts
@@ -3,13 +3,25 @@ import { ClientConfig } from '../interfaces';
 export abstract class BaseClient {
   constructor(protected readonly config: ClientConfig) {}
 
+  /**
+   * Set api token.
+   *
+   * @param apiToken - The api token
+   * @returns The client instance
+   */
   public setApiToken(apiToken: string): this {
     this.config.apiToken = apiToken;
     return this;
   }
 
-  public setApiVersion(version: string): this {
-    this.config.apiVersion = version;
+  /**
+   * Set api version.
+   *
+   * @param apiVersion - The api version
+   * @returns The client instance
+   */
+  public setApiVersion(apiVersion: string): this {
+    this.config.apiVersion = apiVersion;
     return this;
   }
 }
